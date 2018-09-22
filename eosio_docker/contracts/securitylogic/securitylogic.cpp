781718eb620be9c174a7303db34e1431d501c01f
liebeskind <daniel.liebeskind@gmail.com>
2018-09-22 20:26:45 +0100

    Some changes to smart contract

--- eosio_docker/contracts/securitylogic/securitylogic.cpp
+++ eosio_docker/contracts/securitylogic/securitylogic.cpp
@@ -21,9 +21,9 @@ class securitylogic : public eosio::contract {
       pref_table pref_obj(_self, _self);
       // get object by secordary key
       auto preferences = pref_obj.get_index<N(getbyuser)>();
-      auto spend_max = preferences.find(user);
+      auto user_prefs = preferences.find(user);
 
-      return spend_max == preferences.end();
+      return user_prefs == preferences.end();
     }
 
     /// @abi table
@@ -31,6 +31,8 @@ class securitylogic : public eosio::contract {
       uint64_t      prim_key;  // primary key
       account_name  user;      // account name for the user
       uint64_t      spend_max; // max amount that can be spent in a single transaction
+      uint64_t      trans_max; // max number of transactions over time period
+      std::string   time_period; // time period over which transactions are limited by trans_max
       uint64_t      timestamp; // the store the last update block time
 
       // primary key
@@ -48,7 +50,7 @@ class securitylogic : public eosio::contract {
     using contract::contract;
 
     /// @abi action
-    void update( account_name _user, uint64_t& _spend_max ) {
+    void update( account_name _user, uint64_t& _spend_max, uint64_t& _trans_max ) {
       // to sign the action with the given account
       require_auth( _user );
 
@@ -56,24 +58,46 @@ class securitylogic : public eosio::contract {
 
       // create new / update spend_max depends whether the user account exist or not
       if (isnewuser(_user)) {
-        // insert object
-        obj.emplace( _self, [&]( auto& address ) { // Instead of inserting, should replace.
+        // insert object if this is a new user
+        obj.emplace( _self, [&]( auto& address ) {
           address.prim_key    = obj.available_primary_key();
           address.user        = _user;
           address.spend_max   = _spend_max;
+          address.trans_max   = _trans_max;
           address.timestamp   = now();
         });
       } else {
-        // get object by secordary key
+        // get object by secordary key and modify
         auto preferences = obj.get_index<N(getbyuser)>();
-        auto &spend_max = preferences.get(_user);
+        auto &user_prefs = preferences.get(_user);
         // update object
-        obj.modify( spend_max, _self, [&]( auto& address ) {
+        obj.modify( user_prefs, _self, [&]( auto& address ) {
           address.spend_max   = _spend_max;
+          address.trans_max   = _trans_max;
           address.timestamp   = now();
         });
       }
     }
+
+    // /// @abi action
+    // bool approveTransaction( account_name _user, uint64_t& _spent ) {
+    //   // to sign the action with the given account
+    //   require_auth( _user );
+
+    //   pref_table obj(_self, _self); // code, scope
+
+    //     // get object by secordary key and modify
+    //     auto preferences = obj.get_index<N(getbyuser)>();
+    //     auto &user_prefs = preferences.get(_user);
+
+    //     return user_prefs.spend_max > _spent;
+    //     // // update object
+    //     // obj.modify( user_prefs, _self, [&]( auto& address ) {
+    //     //   address.spend_max   = _spend_max;
+    //     //   address.trans_max   = _trans_max;
+    //     //   address.timestamp   = now();
+    //     // });
+    //   }
 
 };
 
