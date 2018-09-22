8eb981c5b610df6e8b34ad3a2c56d42dfa486592
liebeskind <daniel.liebeskind@gmail.com>
2018-09-22 19:05:34 +0100

    Adds moment formatting and changes user names

--- frontend/src/pages/Preferences/index.jsx
+++ frontend/src/pages/Preferences/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Eos from "eosjs"; // https://github.com/EOSIO/eosjs
+import moment from "moment";
 
 // material-ui dependencies
 import { withStyles } from "@material-ui/core/styles";
@@ -18,39 +19,29 @@ import styles from "./styles";
 // This is for demo purposes only!
 const accounts = [
   {
-    name: "usera",
+    name: "daniel",
     privateKey: "5K7mtrinTFrVTduSxizUc5hjXJEtTjVTsqSHeBHes1Viep86FP5",
     publicKey: "EOS6kYgMTCh1iqpq9XGNQbEi8Q6k5GujefN9DSs55dcjVyFAq7B6b"
   },
   {
-    name: "userb",
+    name: "danielle",
     privateKey: "5KLqT1UFxVnKRWkjvhFur4sECrPhciuUqsYRihc1p9rxhXQMZBg",
     publicKey: "EOS78RuuHNgtmDv9jwAzhxZ9LmC6F295snyQ9eUDQ5YtVHJ1udE6p"
   },
   {
-    name: "userc",
+    name: "trish",
     privateKey: "5K2jun7wohStgiCDSDYjk3eteRH1KaxUQsZTEmTGPH4GS9vVFb7",
     publicKey: "EOS5yd9aufDv7MqMquGcQdD6Bfmv6umqSuh9ru3kheDBqbi6vtJ58"
   },
   {
-    name: "userd",
+    name: "annette",
     privateKey: "5KNm1BgaopP9n5NqJDo9rbr49zJFWJTMJheLoLM5b7gjdhqAwCx",
     publicKey: "EOS8LoJJUU3dhiFyJ5HmsMiAuNLGc6HMkxF4Etx6pxLRG7FU89x6X"
   },
   {
-    name: "usere",
+    name: "ashe",
     privateKey: "5KE2UNPCZX5QepKcLpLXVCLdAw7dBfJFJnuCHhXUf61hPRMtUZg",
     publicKey: "EOS7XPiPuL3jbgpfS3FFmjtXK62Th9n2WZdvJb6XLygAghfx1W7Nb"
-  },
-  {
-    name: "userf",
-    privateKey: "5KaqYiQzKsXXXxVvrG8Q3ECZdQAj2hNcvCgGEubRvvq7CU3LySK",
-    publicKey: "EOS5btzHW33f9zbhkwjJTYsoyRzXUNstx1Da9X2nTzk8BQztxoP3H"
-  },
-  {
-    name: "userg",
-    privateKey: "5KFyaxQW8L6uXFB6wSgC44EsAbzC7ideyhhQ68tiYfdKQp69xKo",
-    publicKey: "EOS8Du668rSVDE3KkmhwKkmAyxdBd73B51FKE7SjkKe5YERBULMrw"
   }
 ];
 
@@ -149,14 +140,13 @@ class Preferences extends Component {
           <Typography variant="headline" component="h2">
             {user}
           </Typography>
-          <Typography
-            style={{ fontSize: 12 }}
-            color="textSecondary"
-            gutterBottom
-          >
-            {new Date(timestamp * 1000).toString()}
+          <Typography variant="subheading" component="h2" gutterBottom>
+            {accounts[0].publicKey}
           </Typography>
           <Typography component="pre">Spending Max: ${spend_max}</Typography>
+          <Typography style={{ fontSize: 12 }} color="textSecondary">
+            Last Updated: {moment(new Date(timestamp * 1000)).format("LLL")}
+          </Typography>
         </CardContent>
       </Card>
     );
@@ -175,9 +165,6 @@ class Preferences extends Component {
         </AppBar>
         {noteCards}
         <Paper className={classes.paper}>
-          <Typography variant="subheading" color="inherit">
-            Account Logged In: {accounts[0].publicKey}
-          </Typography>
           <form
             className={classes.formContainer}
             onSubmit={this.handleFormEvent}
