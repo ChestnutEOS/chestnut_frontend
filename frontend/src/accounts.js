a69b9676791bce6e65b6dbc1284aa38d6e22ba72
liebeskind <daniel.liebeskind@gmail.com>
2018-09-23 06:32:19 +0100

    cleanup

---
+++ frontend/src/accounts.js
@@ -0,0 +1,29 @@
+// NEVER store private keys in any source code in your real life development
+// This is for demo purposes only!
+export default [
+  {
+    name: "daniel",
+    privateKey: "5K7mtrinTFrVTduSxizUc5hjXJEtTjVTsqSHeBHes1Viep86FP5",
+    publicKey: "EOS6kYgMTCh1iqpq9XGNQbEi8Q6k5GujefN9DSs55dcjVyFAq7B6b"
+  },
+  {
+    name: "danielle",
+    privateKey: "5KLqT1UFxVnKRWkjvhFur4sECrPhciuUqsYRihc1p9rxhXQMZBg",
+    publicKey: "EOS78RuuHNgtmDv9jwAzhxZ9LmC6F295snyQ9eUDQ5YtVHJ1udE6p"
+  },
+  {
+    name: "trish",
+    privateKey: "5K2jun7wohStgiCDSDYjk3eteRH1KaxUQsZTEmTGPH4GS9vVFb7",
+    publicKey: "EOS5yd9aufDv7MqMquGcQdD6Bfmv6umqSuh9ru3kheDBqbi6vtJ58"
+  },
+  {
+    name: "annette",
+    privateKey: "5KNm1BgaopP9n5NqJDo9rbr49zJFWJTMJheLoLM5b7gjdhqAwCx",
+    publicKey: "EOS8LoJJUU3dhiFyJ5HmsMiAuNLGc6HMkxF4Etx6pxLRG7FU89x6X"
+  },
+  {
+    name: "ashe",
+    privateKey: "5KE2UNPCZX5QepKcLpLXVCLdAw7dBfJFJnuCHhXUf61hPRMtUZg",
+    publicKey: "EOS7XPiPuL3jbgpfS3FFmjtXK62Th9n2WZdvJb6XLygAghfx1W7Nb"
+  }
+];
--- frontend/src/pages/Preferences/index.jsx
+++ frontend/src/pages/Preferences/index.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import Eos from "eosjs"; // https://github.com/EOSIO/eosjs
 import moment from "moment";
-// import ScatterJS from "scatter-js/dist/scatter.esm";
 
-// material-ui dependencies
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -19,36 +17,7 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 
 import styles from "./styles";
 import Header from "../../components/Header";
-
-// NEVER store private keys in any source code in your real life development
-// This is for demo purposes only!
-const accounts = [
-  {
-    name: "daniel",
-    privateKey: "5K7mtrinTFrVTduSxizUc5hjXJEtTjVTsqSHeBHes1Viep86FP5",
-    publicKey: "EOS6kYgMTCh1iqpq9XGNQbEi8Q6k5GujefN9DSs55dcjVyFAq7B6b"
-  },
-  {
-    name: "danielle",
-    privateKey: "5KLqT1UFxVnKRWkjvhFur4sECrPhciuUqsYRihc1p9rxhXQMZBg",
-    publicKey: "EOS78RuuHNgtmDv9jwAzhxZ9LmC6F295snyQ9eUDQ5YtVHJ1udE6p"
-  },
-  {
-    name: "trish",
-    privateKey: "5K2jun7wohStgiCDSDYjk3eteRH1KaxUQsZTEmTGPH4GS9vVFb7",
-    publicKey: "EOS5yd9aufDv7MqMquGcQdD6Bfmv6umqSuh9ru3kheDBqbi6vtJ58"
-  },
-  {
-    name: "annette",
-    privateKey: "5KNm1BgaopP9n5NqJDo9rbr49zJFWJTMJheLoLM5b7gjdhqAwCx",
-    publicKey: "EOS8LoJJUU3dhiFyJ5HmsMiAuNLGc6HMkxF4Etx6pxLRG7FU89x6X"
-  },
-  {
-    name: "ashe",
-    privateKey: "5KE2UNPCZX5QepKcLpLXVCLdAw7dBfJFJnuCHhXUf61hPRMtUZg",
-    publicKey: "EOS7XPiPuL3jbgpfS3FFmjtXK62Th9n2WZdvJb6XLygAghfx1W7Nb"
-  }
-];
+import accounts from "../../accounts";
 
 // Preferences component
 class Preferences extends Component {
@@ -64,25 +33,16 @@ class Preferences extends Component {
     this.handleFormEvent = this.handleFormEvent.bind(this);
   }
 
-  // generic function to handle form events (e.g. "submit" / "reset")
-  // push transactions to the blockchain by using eosjs
   async handleFormEvent(event) {
-    // stop default behaviour
     event.preventDefault();
-
-    // collect form data
-    // let account = event.target.account.value;
     let account = accounts[0].name;
-    // let privateKey = event.target.privateKey.value;
     let privateKey = accounts[0].privateKey;
     let spend_max = event.target.spend_max.value;
     let trans_max = event.target.trans_max.value;
 
-    // prepare variables for the switch below to send transactions
     let actionName = "";
     let actionData = {};
 
-    // define actionName and action according to event type
     switch (event.type) {
       case "submit":
         actionName = "update";
@@ -96,7 +56,6 @@ class Preferences extends Component {
         return;
     }
 
-    // eosjs function call: connect to the blockchain
     const eos = Eos({ keyProvider: privateKey });
     const result = await eos.transaction({
       actions: [
@@ -116,14 +75,6 @@ class Preferences extends Component {
 
     this.getTable();
   }
-
-  // sendEos = amount => {
-  //   const eos = Eos();
-  //   eos.transfer("userB", "helloworld", "1.0000 EOS", "memo");
-  // };
-
-  // gets table data from the blockchain
-  // and saves it into the component state: "prefTable"
   getTable = () => {
     const eos = Eos();
     eos
