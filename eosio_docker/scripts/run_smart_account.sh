958c75f58f8a67c1a581d0e24be74de01d5c8f6f
jackdisalvatore <jackp.disalvatore@gmail.com>
2018-11-24 20:13:18 -0500

    demonstrating account freeze

--- eosio_docker/scripts/run_smart_account.sh
+++ eosio_docker/scripts/run_smart_account.sh
@@ -6,7 +6,12 @@ cleos push action smartaccount update '["smartaccount","0"]' -p smartaccount
 echo '=== cleos get table smartaccount smartaccount settings ==='
 cleos get table smartaccount smartaccount settings
 
-#echo '=== freeze account ==='
-#cleos push action smartaccount update '["smartaccount","1"]' -p smartaccount
-#cleos get table smartaccount smartaccount settings
+echo '=== freeze account ==='
+cleos push action smartaccount update '["smartaccount","1"]' -p smartaccount
+echo '=== cleos get table smartaccount smartaccount settings ==='
+cleos get table smartaccount smartaccount settings
+# cleos push action smartaccount hello '[]' -p smartaccount
+sleep 1
+echo '=== unfreeze account ==='
+cleos push action smartaccount update '["smartaccount","0"]' -p smartaccount
 cleos push action smartaccount hello '[]' -p smartaccount
