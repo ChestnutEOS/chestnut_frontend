d1c5c164648ccc3aa7a62954a8d87cb0ae5ccfee
liebeskind <daniel.liebeskind@gmail.com>
2018-11-25 17:05:42 -0800

    Adds favicon and begins adding DFuse

---
+++ frontend/src/utils/dfuse-client.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+// Dfuse endpoint
+const endpoint = "https://mainnet.eos.dfuse.io/";
+const apiKey = "Insert";
+
+class DFuseClient extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+}
+
+export default DFuseClient;
